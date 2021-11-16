e9c1a6ace86c4bc3c6f02b58cf0c1061410552c1
Yyyyyyhjjjaja <94437553+Yyyyyyhjjjaja@users.noreply.github.com>
2021-11-16 14:48:05 +0700

    Update core.py

--- userbot/core.py
+++ userbot/core.py
@@ -56,6 +56,262 @@ async def send(event):
 
 @command(pattern=r"^.unload (?P<shortname>\w+)$", outgoing=True)
 async def unload(event):
+ CHAT TO CALZ ANGELS
+Tiaraa ᥴꪖ ༊:
+Pey ngambek anjj
+
+Lu siii ngajak tmoan
+
+icaaa ᥴꪖ ༊:
+tolol
+
+yagahahaha
+
+lo juga ikut bae
+
+Tiaraa ᥴꪖ ༊:
+Dia bilng
+
+"udh lu gausa ganggu gua dulu"
+
+Gtuuu😭
+
+Nyampe skrng ga cht cht
+
+Hel ᥴꪖ ༊:
+Anjort
+
+Crtanya lgi break nih?
+
+glo ᥴꪖ ༊:
+hii gess
+
+icaaa ᥴꪖ ༊:
+YAHSHAHA
+
+glo ᥴꪖ ༊:
+oit
+
+icaaa ᥴꪖ ༊:
+kliling yu
+
+Tiaraa ᥴꪖ ༊:
+Naik publik
+
+icaaa ᥴꪖ ༊:
+@joinsinitodd
+
+𝐁𝐎𝐓 𝐀𝐍𝐆𝐄𝐋𝐒:
+Welkaaamm Cantik Farida
+
+Lalaapoooo:
+Hayuuuu
+
+icaaa ᥴꪖ ༊:
+.
+
+Lalaapoooo:
+Buruu
+
+Hel ᥴꪖ ༊:
+Ntar gw silah dulu
+
+Lalaapoooo:
+Lahhh tmonya di udahi
+
+n
+
+icaaa ᥴꪖ ༊:
+ayo naik
+
+buru
+
+angkat ketek😭
+
+koo dipilih tod😭😭
+
+Lalaapoooo:
+Hahaha ngakak anj di pilih
+
+Hhahaahha
+
+icaaa ᥴꪖ ༊:
+je pilih jaki
+
+Lalaapoooo:
+Yang mnaa
+
+icaaa ᥴꪖ ༊:
+jakipan
+
+Lalaapoooo:
+Mnaaa naikin mknyaaa anj ko ga naik naik
+
+icaaa ᥴꪖ ༊:
+sabarr tod
+
+Lalaapoooo:
+Hhaahah ngakak
+
+icaaa ᥴꪖ ༊:
+ntr gua pasti roast di call sm jaki😭😭
+
+TOLOL NGENTOD
+
+nama siapa asal dimana umur berapa
+
+malah nanya ngntf
+
+Lalaapoooo:
+Hahaha ngakak anj😭😭😭😭😭
+
+icaaa ᥴꪖ ༊:
+ganti akun yu😭
+
+milih lagi😭
+
+TOLOL MINTA Mute😭
+
+Lalaapoooo:
+Ywdaa gassssssss
+
+Hahaha
+
+icaaa ᥴꪖ ༊:
+gua pake tell. akun ini gbsa gnti pp lg ajg delay
+
+hayuu je
+
+Tiaraa ᥴꪖ ༊:
+Lemott guaaa
+
+Waitt
+
+icaaa ᥴꪖ ༊:
+yu angkat
+
+ngentod lagi nanya☺️
+
+Tiaraa ᥴꪖ ༊:
+Di taekin langsung anj
+
+Anj di skip sama yng jlk
+
+icaaa ᥴꪖ ༊:
+HAHAHAHAGAHA
+
+wibu skip gua☺️
+
+Tiaraa ᥴꪖ ༊:
+Ngentodddd
+
+Hahahahahaha
+
+icaaa ᥴꪖ ༊:
+@algeriozleader
+
+jamet dl
+
+Tiaraa ᥴꪖ ༊:
+Udhh naik
+
+Eh salah akun
+
+icaaa ᥴꪖ ༊:
+tolol
+
+mau pilih log rush ah
+
+Lalaapoooo:
+Naik ga ni
+
+icaaa ᥴꪖ ༊:
+naik dong
+
+Lalaapoooo:
+Gua pilih sape annnj
+
+icaaa ᥴꪖ ༊:
+ntr gua cariin
+
+rzor tuu
+
+eh turun
+
+host nya
+
+Lalaapoooo:
+Ada ada
+
+Orgnya jelek kalo dia skip gua katain
+
+Hahahaha
+
+icaaa ᥴꪖ ༊:
+ngntd😭😭
+
+Lalaapoooo:
+Naikin yang ga angkat tangan babi
+
+icaaa ᥴꪖ ༊:
+kntl emg
+
+gua pc ah adminnya
+
+Lalaapoooo:
+Lamaaa ah babiii
+
+icaaa ᥴꪖ ༊:
+gua pc
+
+Lalaapoooo:
+Mentang mentang gua jele
+
+icaaa ᥴꪖ ༊:
+iyaa ajg
+
+mending pc☺️
+
+najisss nadanya kek males😱
+
+Lalaapoooo:
+Haahahaha
+
+Ngakka
+
+Mampusss
+
+icaaa ᥴꪖ ༊:
+pc dlu emm
+
+Lalaapoooo:
+Males anjj
+
+icaaa ᥴꪖ ༊:
+di bles ajg😭
+
+Lalaapoooo:
+Hah seriuss
+
+Bhaks
+
+Maless ah anjjj gua pake akun pinjol wait
+
+icaaa ᥴꪖ ༊:
+udah gua pc in ajg
+
+sabar
+
+katanya abis ini lo
+
+Tiaraa ᥴꪖ ༊:
+Hhaaahah 😭😭
+
+Ngebugg hp gua
+
+Anjinggggg dahla
+
+Kesel
     if event.fwd_from:
         return
     shortname = event.pattern_match["shortname"]
